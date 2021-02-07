bc167e4f6426a0379dfe1a9066e7ef187a5eaf4d
kousuke1201abe <k-abe@giftee.co>
2021-02-07 23:11:36 +0900

    repository に書いてある DB 接続系のロジックを infrastructure に移動

--- domain/repository/todo.go
+++ domain/repository/todo.go
@@ -1,17 +1,6 @@
 package repository
 
-import (
-	"github.com/jinzhu/gorm"
-	"github.com/kousuke1201abe/gqlgen-todos/domain/model"
-)
-
-func (d *TodoDao) TableName() string {
-	return "todos"
-}
-
-type TodoDao struct {
-	db *gorm.DB
-}
+import "github.com/kousuke1201abe/gqlgen-todos/domain/model"
 
 type TodoRepository interface {
 	Create(u *model.Todo) (*model.Todo, error)
@@ -20,62 +9,4 @@ type TodoRepository interface {
 	Find(id string) (*model.Todo, error)
 	Destroy(t *model.Todo) error
 	Save(t *model.Todo) error
-}
-
-func NewTodoRepository(db *gorm.DB) TodoRepository {
-	return &TodoDao{db: db}
-}
-
-func (d *TodoDao) Create(t *model.Todo) (*model.Todo, error) {
-	res := d.db.Create(t)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	return t, nil
-}
-
-func (d *TodoDao) All() ([]*model.Todo, error) {
-	var todos []*model.Todo
-	res := d.db.Find(&todos)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	return todos, nil
-}
-
-func (d *TodoDao) Find(id string) (*model.Todo, error) {
-	var todos []*model.Todo
-	res := d.db.Where("id = ?", id).Find(&todos)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	if len(todos) < 1 {
-		return nil, nil
-	}
-	return todos[0], nil
-}
-
-func (d *TodoDao) FindByUserID(userID string) ([]*model.Todo, error) {
-	var todos []*model.Todo
-	res := d.db.Where("user_id = ?", userID).Find(&todos)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	return todos, nil
-}
-
-func (d *TodoDao) Destroy(t *model.Todo) error {
-	res := d.db.Delete(t)
-	if err := res.Error; err != nil {
-		return err
-	}
-	return nil
-}
-
-func (d *TodoDao) Save(t *model.Todo) error {
-	res := d.db.Save(t)
-	if err := res.Error; err != nil {
-		return err
-	}
-	return nil
 }
--- domain/repository/user.go
+++ domain/repository/user.go
@@ -1,70 +1,10 @@
 package repository
 
-import (
-	"github.com/jinzhu/gorm"
-	"github.com/kousuke1201abe/gqlgen-todos/domain/model"
-)
-
-func (d *UserDao) TableName() string {
-	return "users"
-}
-
-type UserDao struct {
-	db *gorm.DB
-}
+import "github.com/kousuke1201abe/gqlgen-todos/domain/model"
 
 type UserRepository interface {
 	Create(u *model.User) (*model.User, error)
 	All() ([]*model.User, error)
 	FindByTodoID(todoID string) (*model.User, error)
 	Find(id string) (*model.User, error)
-}
-
-func NewUserRepository(db *gorm.DB) UserRepository {
-	return &UserDao{db: db}
-}
-
-func (d *UserDao) Create(u *model.User) (*model.User, error) {
-	res := d.db.Create(u)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	return u, nil
-}
-
-func (d *UserDao) All() ([]*model.User, error) {
-	var Users []*model.User
-	res := d.db.Find(&Users)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	return Users, nil
-}
-
-func (d *UserDao) Find(id string) (*model.User, error) {
-	var Users []*model.User
-	res := d.db.Where("id = ?", id).Find(&Users)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	if len(Users) < 1 {
-		return nil, nil
-	}
-	return Users[0], nil
-}
-
-func (d *UserDao) FindByTodoID(todoID string) (*model.User, error) {
-	var users []*model.User
-	res := d.db.Table("users").
-		Select("users.*").
-		Joins("LEFT JOIN todos ON todos.user_id = users.id").
-		Where("todos.id = ?", todoID).
-		First(&users)
-	if err := res.Error; err != nil {
-		return nil, err
-	}
-	if users == nil || len(users) == 0 {
-		return nil, nil
-	}
-	return users[0], nil
 }
